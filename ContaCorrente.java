709c0bd15ce2edb0c880bae895b0403451e8cb66
Andrey_Del_Bello <andrey.delbello2002@gmail.com>
2021-11-11 19:38:07 +0000

    Conta

---
+++ ContaCorrente.java
@@ -0,0 +1,53 @@
+public class ContaCorrente {
+    //numero, saldo status
+    private int numero;
+    private double saldo;
+    private boolean status;
+    //status true estou no cheque especial
+    //status false estou com a conta no azul
+    private double limite;
+
+    public void saca(double valor){
+        
+        if(valor > saldo && limite < 2000){
+            status=true;
+            limite+=saldo-valor;
+            saldo=saldo-valor;
+        }
+        else{
+            status=false;
+        }
+        
+    }
+
+    public void deposita(double valor){
+        saldo+=valor;
+        //saldo=saldo+valor
+    }
+    
+    public double getLimite() {
+        return limite;
+    }
+    public void setLimite(double limite) {
+        this.limite = limite;
+    }
+    public int getNumero() {
+        return numero;
+    }
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+    public double getSaldo() {
+        return saldo;
+    }
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+    public boolean isStatus() {
+        return status;
+    }
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+    
+}
